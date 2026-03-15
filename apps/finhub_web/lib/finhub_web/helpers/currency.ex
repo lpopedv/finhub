34982b771610aeb95b50a544d9d6d1e701161ea2
Lucas Pope <lpopedv@proton.me>
2026-03-15 10:42:19 -0300

    Make credo 😁

--- apps/finhub_web/lib/finhub_web/helpers/currency.ex
+++ apps/finhub_web/lib/finhub_web/helpers/currency.ex
@@ -2,7 +2,13 @@ defmodule FinhubWeb.Helpers.Currency do
   @moduledoc "Formatting helpers for monetary values stored as integer cents."
 
   @spec format_brl(integer()) :: String.t()
-  def format_brl(cents),
-    do:
-      "R$ #{div(cents, 100)},#{rem(cents, 100) |> Integer.to_string() |> String.pad_leading(2, "0")}"
+  def format_brl(cents) do
+    decimals =
+      cents
+      |> rem(100)
+      |> Integer.to_string()
+      |> String.pad_leading(2, "0")
+
+    "R$ #{div(cents, 100)},#{decimals}"
+  end
 end
